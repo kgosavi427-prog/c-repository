cb67fe3a67aa76dc1c9ea3c42c4fc2afd7a8ff74
kgosavi427-prog <kgosavi427@gmail.com>
2026-07-09 16:51:10 +0530

    Add files via upload

---
+++ If.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+	int age;
+	printf("Enter Your Age: ");
+	scanf("%d",&age);
+	 if(age>=18){
+	 	printf("Adult and Eligible for Voting");
+	 	}
+	 	else{
+	 		printf("Not Adult");
+		 }
+		 return 0;
+}
