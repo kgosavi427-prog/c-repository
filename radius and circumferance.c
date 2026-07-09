4eba0d00d5223639f6bfb3a70c5595102b4bd782
kgosavi427-prog <kgosavi427@gmail.com>
2026-07-09 16:23:37 +0530

    Add files via upload

---
+++ radius and circumferance.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	float D;
+	printf("Enter Diameter : ");
+	scanf("%f",&D);
+	 
+	
+	printf("Area of Circle %f ",3.14*D/2*D/2);
+	
+	printf("\n Circumferance of Circle %f",2*3.14*D/2);
+	return 0;
+}
